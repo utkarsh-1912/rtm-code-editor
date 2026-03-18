3882a40bf3a0144f17d802c7359a36785191bba6
Utkarsh Gupta <79002186+utkarsh-1912@users.noreply.github.com>
2026-03-18 19:29:57 +0530

    v1.0.0

--- src/components/VideoChat.js
+++ src/components/VideoChat.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef, useState, useCallback } from 'react';
 import {
     PhoneOff, Video, VideoOff, Mic, MicOff, Maximize2, Minimize2,
-    ChevronDown, User, ScreenShare, ScreenShareOff
+    ChevronDown, User, ScreenShare, ScreenShareOff, hangUpStyle
 } from 'lucide-react';
 import toast from 'react-hot-toast';
 import ACTIONS from '../Action';
